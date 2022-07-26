e20c4026038d05981168c812be07f28c4183d7a2
zack77 <aguszakariya@gmail.com>
2022-07-26 18:45:30 +0800

    update index.php
    update

--- index.php
+++ index.php
@@ -32,7 +32,6 @@ if (isset($_FILES['attachments'])) {
 <style>
     body{
         font-family: 'Roboto Slab', serif;
-        font-family: 'Roboto', sans-serif;
         background-image: url("https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
         background-size: cover;
         background-repeat: no-repeat;
